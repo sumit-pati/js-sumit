489e5bd459326db21d981600b8f9f5694972b4e7
Sumit Pati <57040914+sumit-pati@users.noreply.github.com>
2025-10-30 20:14:00 +0000

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all JS as newer version
+
+// alert (3 + 3 )  // we are using newer nodejs, not browser
+
+console.log(3
+    +
+    3
+); // Code readibility should be high
+
+console.log("Sumit");
+
+let name = "Sumit"
+let age = 25
+let isLoggedIn = false
+let state
+
+// number => 2 to poewr 53
+// bigint
+// string  => ""
+// boolean => trur/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof symbol);
+
