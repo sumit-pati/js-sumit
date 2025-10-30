15e361a8f0081bc30eb302a9a2d84a29bbf935bd
Sumit Pati <57040914+sumit-pati@users.noreply.github.com>
2025-10-30 20:29:03 +0000

    Conversion of Data Types

---
+++ 01_Basics/03_conversionsOperation.js
@@ -0,0 +1,29 @@
+let score = "Sumit"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 4
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0  => false
+// "" => false
+// "Sumit" => true
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(typeof(stringSomeNumber));
+console.log(stringSomeNumber);
