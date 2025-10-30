69ad4097c837c0a2e9464ea01d15944780ebfee5
Sumit Pati <57040914+sumit-pati@users.noreply.github.com>
2025-10-30 20:44:39 +0000

    Operations

--- 01_Basics/03_conversionsOperation.js
+++ 01_Basics/03_conversionsOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = "Sumit"
 
 console.log(typeof score);
@@ -14,7 +15,6 @@ console.log(valueInNumber);
 
 
 let isLoggedIn = 4
-
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(typeof(booleanIsLoggedIn));
 console.log(booleanIsLoggedIn);
@@ -27,3 +27,41 @@ let someNumber = 33
 let stringSomeNumber = String(someNumber)
 console.log(typeof(stringSomeNumber));
 console.log(stringSomeNumber);
+*/
+/**************************** Operations ******************************/
+
+let value = 3
+let negValue  = - value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " Sumit!"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);       // 12
+// console.log(1 + "2");       // 12
+// console.log(1 + "2" + 2);   // 122
+// console.log(1 + 2 + "2");   // 32
+
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true); // 1
+// console.log(+"");   // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++ gameCounter
+console.log(gameCounter);
