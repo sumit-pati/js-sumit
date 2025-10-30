d21edeb8c4c53a9a2acd7eb34c38b3c3d3287284
Sumit Pati <57040914+sumit-pati@users.noreply.github.com>
2025-10-30 21:14:35 +0000

    data-type summary

---
+++ 01_Basics/02.1_datatypesSummary.js
@@ -0,0 +1,35 @@
+// Premative
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 78923414567897653n
+
+
+
+// Referance (Nim Premitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktman", "naagraj", "doga"];
+let obj = {
+    name: "Sumit",
+    age: 26
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros);
